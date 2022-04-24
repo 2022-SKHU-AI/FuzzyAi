5b26f385692533aa9554437a98fdf4377b028fa7
SonChangHa <jjang00198@gmail.com>
2022-04-24 16:43:40 +0900

    진짜 최종

--- fuzzy.py
+++ fuzzy.py
@@ -17,15 +17,15 @@ rankTable = ['인턴', '사원', '주임', '대리', '계장', '과장', '차장
 
 if __name__ == "__main__":
     try:
-        speaker = int(input("하는 사람의 나이 입력: "))
-        #speaker = 30
-        listener = int(input("듣는 사람의 나이 입력: "))
-        #listener = 60
-        rank = input("직급 입력(과장, 부장 등..): ")
-        #rank = "부장"
+        #speaker = int(input("하는 사람의 나이 입력: "))
+        speaker = 30
+        #listener = int(input("듣는 사람의 나이 입력: "))
+        listener = 60
+        #rank = input("직급 입력(과장, 부장 등..): ")
+        rank = "부장"
         rankValue = rankTable.index(rank)
-        look = list(input("표정 입력(분노 경멸 혐오 행복의 형태로 입력.\n입력 예시 : 0.05 0.03 0.02 0.55): ").split())
-        #look = "0.05 0.03 0.02 0.55".split()
+        #look = list(input("표정 입력(분노 경멸 혐오 행복의 형태로 입력.\n입력 예시 : 0.05 0.03 0.02 0.55): ").split())
+        look = "0.05 0.03 0.02 0.55".split()
         lookValue = -(float(look[0])+float(look[1])+float(look[2]))+float(look[3])
     except Exception as e:
         print(e)
@@ -74,7 +74,7 @@ if __name__ == "__main__":
     for x in numpy.arange(0.0, 100.01, 0.1):
         temp = az_.value(x) # x값에 따른 후건부 소속함수 값 확인
         y = []
-# 머지
+
         # temp값과 퍼지출력값을 비교하여 퍼지가 크면 temp를, 아니면 반대로 값 추가
         y.append(temp[0] if fuzzySet[0] > temp[0] else fuzzySet[0])
         y.append(temp[1] if fuzzySet[1] > temp[1] else fuzzySet[1])
@@ -101,8 +101,10 @@ if __name__ == "__main__":
         # 근데 그럼 시간이 오래걸림
         sum += max(y) * x
 
-        denominator += max(y) # 분모에 가로 길이를 더하는거 아님??
+        denominator += max(y)# 분모에 가로 길이를 더하는거 아님??
 
+
+    #denominator = 100  # 분모에 가로 길이를 더하는거 아님??
 
     plt.show() # 그래프 표시
 
